d592a1b1c6c589f8ec584ca1e6bcb21165274d9f
Shahriar262 <srh.2622@gmail.com>
2025-12-17 11:28:56 +0600

    added Loading Spinner

--- src/Components/HomePage/ContactCTA.jsx
+++ src/Components/HomePage/ContactCTA.jsx
@@ -1,11 +1,11 @@
 const ContactCTA = () => {
   return (
-    <section className="bg-indigo-800 text-white py-16 text-center">
+    <section className="bg-indigo-700 text-white py-16 text-center">
       <h2 className="text-3xl font-semibold">Ready to Get Started?</h2>
       <p className="mt-3">Contact us or start managing your assets today.</p>
       <a
         href="/register-hr"
-        className="btn bg-indigo-400 mt-6 hover:bg-indigo-500 transition"
+        className="btn bg-indigo-300 mt-6 hover:bg-indigo-400 transition"
       >
         Start Now
       </a>
